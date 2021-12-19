7b5ccb2576f84c906a173bff50f59a7fd52c73be
JVKran <jochem.vankranenburg@outlook.com>
2021-12-19 16:56:09 +0100

    Pretty far already.

--- led_test/main.c
+++ led_test/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #include <unistd.h>
 #include <fcntl.h>
@@ -10,13 +11,20 @@
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 
-#define LED_FILE	"/dev/leds_test_module"
-#define SSD_FILE	"/dev/ssd_test_module"
-#define RTM_FILE	"/dev/response_module"
-#define RTM_IRQ		10
+#define LED_FILE		"/dev/leds_test_module"
+#define SSD_FILE		"/dev/ssd_test_module"
+#define RTM_FILE		"/dev/response_module"
+#define RTM_IRQ			10
+
+#define MIN_RESP_TIME	80
 
 static bool exit_request = false;
+uint16_t highscore = UINT16_MAX;
 static int fd;
+
+enum states {IDLE, START, DELAY, COUNT, STOP};
+enum states state = IDLE;
+int rtm_state = 0;
 
 void exit_handler(int n, siginfo_t *info, void *unused){
 	exit_request = (n == SIGINT);
@@ -26,10 +34,11 @@ void exit_handler(int n, siginfo_t *info, void *unused){
 void response_irq(int n, siginfo_t *info, void *unused){
     if (n == 10 && info->si_int != 0) {
         printf("Interrupt received with state %i.\n", info->si_int);
+		rtm_state = info->si_int;
     }
 }
 
-int control_leds(const int value){
+int control_leds(const uint32_t value){
     FILE * fp = fopen(LED_FILE, "a");
     if(fp == NULL){
         return 1;
@@ -39,7 +48,7 @@ int control_leds(const int value){
     return fclose(fp);
 }
 
-int control_ssd(const int value){
+int control_ssd(const uint32_t value){
 	FILE * fp = fopen(SSD_FILE, "a");
     if(fp == NULL){
         return 1;
@@ -52,24 +61,28 @@ int control_ssd(const int value){
 int init_rtm(int * number){
 	fd = open(RTM_FILE, O_RDWR);
     if(fd < 0) {
-		printf("Device bestand niet kunnen openen..\n");
 		return fd;
     }
  
-    printf("Applicatie registreren\n");
+    printf("Registering application\n");
     if (ioctl(fd, _IOW('a','a',int32_t*) ,(int32_t*)number)) {
-        printf("IOCTL magie fout\n");
+        printf("IOCTL error!\n");
         close(fd);
         exit(1);
     }
    
-    printf("Wacht op signaal...\n");
+    printf("Waiting for interrupts.\n");
 	return 0;
+}
+
+int rotate_left(int num, int shift){
+    return (num << shift) | (num >> (9 - shift));
 }
 
 int main(){
 	int err = 0, number;
 	struct sigaction signal;
+	uint16_t response_time, leds = 0;
 
 	// Register ctrl+c handler
 	sigemptyset(&signal.sa_mask);
@@ -83,18 +96,74 @@ int main(){
     signal.sa_sigaction = response_irq;
     sigaction(RTM_IRQ, &signal, NULL);
 
-	init_rtm(&number);
+	// Initialize response time meter
+	err = init_rtm(&number);
+	if(err < 0){
+		return err;
+	}
 
-	// Count up to 16 or until exit request
-    for(int counter = 0; counter < 100 && !exit_request; counter++){
-		printf("Showing %i on leds and display.\n", counter);
-        err = control_leds(counter);
-		err |= control_ssd(counter);
-        if(err != 0){
-            break;
-        }
-        usleep(100000);
-    }
+	while (!exit_request){
+		switch(state){
+			case IDLE: {
+				if(highscore != UINT16_MAX){
+					control_ssd(highscore);
+				} else {
+					control_ssd(0);
+				}
+				control_leds(leds);
+				if(rtm_state == 1){
+					state = START;
+				}
+				break;
+			}
+			case START: {
+				control_ssd(0);
+				if(rtm_state == 3){
+					state = COUNT;
+				}
+				break;
+			}
+			case COUNT: {
+				enum states next_state = IDLE;
+				for(uint16_t time = 0; time < 1000; time++){
+					if(time % 100 == 0){
+						leds <<= 1;
+						leds |= 1UL;
+						control_leds(leds);
+					}
+					if(rtm_state == 4){
+						// TODO: read response time from register.
+						response_time = time;
+						if(response_time >= MIN_RESP_TIME){
+							printf("Responded within %ims!\n", response_time);
+							next_state = STOP;
+						}
+						break;
+					}
+					usleep(1000);
+				}
+				state = next_state;
+				break;
+			}
+			case STOP: {
+				control_ssd(response_time);
+				bool new_highscore = false;
+				if(response_time < highscore){
+					highscore = response_time;
+					new_highscore = true;
+				}
+				while(rtm_state != 0){
+					if(new_highscore){
+						leds = rotate_left(leds, 1);
+						control_leds(leds);
+					}
+					usleep(100000);			// Sleep for 100ms.
+				}
+				state = IDLE;
+				break;
+			}
+		}
+	}
 
 	// Reset leds and displays if possible
 	if(!err){
