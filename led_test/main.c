28d0109e3498c5454b62c45ab64507baac54fd48
JVKran <jochem.vankranenburg@outlook.com>
2021-12-19 16:27:37 +0100

    Using all kernel modules in one app

--- led_test/main.c
+++ led_test/main.c
@@ -1,61 +1,106 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 #include <unistd.h>
-#include <stdint.h>
 #include <fcntl.h>
-#include <sys/mman.h>
+#include <signal.h>
 
-#define HW_REGS_BASE 	( 0xff200000 )
-#define HW_REGS_SPAN 	( 0x00200000 )
-#define HW_REGS_MASK 	( HW_REGS_SPAN - 1 )
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
 
-#define LEDS_BASE 		( 0x00010 )
-#define MEAS_BASE 		( 0x00000 )
+#define LED_FILE	"/dev/leds_test_module"
+#define SSD_FILE	"/dev/ssd_test_module"
+#define RTM_FILE	"/dev/response_module"
+#define RTM_IRQ		10
 
-#define REG_BASE 		LEDS_BASE
+static bool exit_request = false;
+static int fd;
 
-#define HWREG(x) (*((volatile uint32_t *)(x)))
+void exit_handler(int n, siginfo_t *info, void *unused){
+	exit_request = (n == SIGINT);
+	printf(" pressed so execution is interrupted!\n");
+}
 
-int main(void){
-	volatile unsigned int *h2p_lw_led_addr=NULL;
-	void *virtual_base;
-	int fd;
+void response_irq(int n, siginfo_t *info, void *unused){
+    if (n == 10 && info->si_int != 0) {
+        printf("Interrupt received with state %i.\n", info->si_int);
+    }
+}
 
-	// Open /dev/mem
-	if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) {
-			printf( "ERROR: could not open \"/dev/mem\"...\n" );
-			return( 1 );
-	}
+int control_leds(const int value){
+    FILE * fp = fopen(LED_FILE, "a");
+    if(fp == NULL){
+        return 1;
+    }
 
-	// Hiermee koppelen we het fysieke geheugen aan het virtuele geheugen
-	// en krijgen we de pointer terug
-	virtual_base = mmap( NULL, HW_REGS_SPAN, ( PROT_READ | PROT_WRITE ),
-						MAP_SHARED, fd, HW_REGS_BASE );
-	if( virtual_base == MAP_FAILED ) {
-		printf( "ERROR: mmap() failed...\n" );
-		close( fd );
-		return(1);
-	}
+    fprintf(fp, "%i", value);
+    return fclose(fp);
+}
 
-	HWREG(virtual_base + REG_BASE) = 0;
+int control_ssd(const int value){
+	FILE * fp = fopen(SSD_FILE, "a");
+    if(fp == NULL){
+        return 1;
+    }
 
-	uint8_t teller =0;
-	while(teller++<255){
+    fprintf(fp, "%i", value);
+    return fclose(fp);
+}
 
-		//knop ingedrukt dus verder
-		printf("Ophogen... %d\n", teller);
-
-		// Add 1 to the PIO register
-		HWREG(virtual_base + REG_BASE) += 1;
-		usleep(1000000);
-	}
-
-	if( munmap( virtual_base, HW_REGS_SPAN ) != 0 ) {
-		printf( "ERROR: munmap() failed...\n" );
-		close( fd );
-		return( 1 );
-	}
-
-	close( fd );
+int init_rtm(int * number){
+	fd = open(RTM_FILE, O_RDWR);
+    if(fd < 0) {
+		printf("Device bestand niet kunnen openen..\n");
+		return fd;
+    }
+ 
+    printf("Applicatie registreren\n");
+    if (ioctl(fd, _IOW('a','a',int32_t*) ,(int32_t*)number)) {
+        printf("IOCTL magie fout\n");
+        close(fd);
+        exit(1);
+    }
+   
+    printf("Wacht op signaal...\n");
 	return 0;
 }
 
+int main(){
+	int err = 0, number;
+	struct sigaction signal;
+
+	// Register ctrl+c handler
+	sigemptyset(&signal.sa_mask);
+    signal.sa_flags = (SA_SIGINFO | SA_RESETHAND);
+    signal.sa_sigaction = exit_handler;
+    sigaction(SIGINT, &signal, NULL);
+
+	// Register handler for response interrupts
+	sigemptyset(&signal.sa_mask);
+    signal.sa_flags = (SA_SIGINFO | SA_RESTART);
+    signal.sa_sigaction = response_irq;
+    sigaction(RTM_IRQ, &signal, NULL);
+
+	init_rtm(&number);
+
+	// Count up to 16 or until exit request
+    for(int counter = 0; counter < 100 && !exit_request; counter++){
+		printf("Showing %i on leds and display.\n", counter);
+        err = control_leds(counter);
+		err |= control_ssd(counter);
+        if(err != 0){
+            break;
+        }
+        usleep(100000);
+    }
+
+	// Reset leds and displays if possible
+	if(!err){
+		err = control_leds(0);
+		err |= control_ssd(0);
+	}
+	close(fd);
+    return err;
+}
--- leds_kernel_module/README.md
+++ leds_kernel_module/README.md
@@ -1,1 +1,1 @@
-Use ```head -c4 /dev/leds*``` to read state of switches and use ```echo "0x02" > /dev/leds*``` to turn on second led.
+Use ```echo "0x02" > /dev/leds*``` to turn on second led.
